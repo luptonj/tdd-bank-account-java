39bb4c27d7566c1de4879f550745379b4ec08505
luptonj <jamesfhlupton@gmail.com>
2021-01-27 21:57:38 +0000

    test I can commit

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertThat;
 public class AccountTest {
 
     @Test
-	@Ignore
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test isn't implemented", true, is(false));
     }
